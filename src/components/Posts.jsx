635c12452d16390aca9689ddf7712cfd3384adae
Shadaf Hossain <shadafhossain01@gmail.com>
2026-01-09 08:17:25 +0600

    Fetch Post Data

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Posts = () => {
-  return <div className="mt-[50px]">Posts</div>;
+    const [posts,setPosts]=useState([])
+    const url = "http://localhost:3000/";
+  
+    // For Getting Data
+      async function fetchPost() {
+        await fetch(url + "posts")
+          .then((res) => res.json())
+          .then((data) => setPosts(data));
+      }
+      
+    useEffect(()=>{
+      fetchPost();
+    },[])
+
+  return (
+    <div className="mt-[50px] grid grid-cols-3 gap-y-4 gap-x-5">
+      {posts.map((item) => (
+        <div
+          className="border border-[#ffffff85] px-5 py-5 rounded-[20px]"
+          key={item.id}
+        >
+          <h2 className="font-bold text-[#ffffff] text-[21px] mb-[15px]">
+            Tittle : {item.tittle}
+          </h2>
+          <p className="font-medium text-[#ffffffb6] text-[21px]">
+            {item.desc}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
